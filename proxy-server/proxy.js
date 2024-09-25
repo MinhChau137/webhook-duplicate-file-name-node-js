c9f82870d125ad117994266bb07e34ee8a574a11
MinhChau137 <c0963018619@gmail.com>
2024-09-25 16:31:12 +0700

    k

--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -15,7 +15,7 @@ app.use('/', proxy(minioWebUrl, {
             // Chèn mã JavaScript hiển thị alert
             html = html.replace('</body>', `
                 <script>
-                    const socket = new WebSocket('ws://localhost:3000');
+                    const socket = new WebSocket('ws://10.39.153.13:3000');
 
                     socket.onmessage = function(event) {
                         const alertMessage = event.data;
